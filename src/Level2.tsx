e19ae55461a87bdb0fdb173238c5d4e747810041
Steph OHara <soh_la@yahoo.com>
2023-01-13 15:49:05 +1100

    motion request bug fixed

--- src/Level2.tsx
+++ src/Level2.tsx
@@ -58,17 +58,17 @@ playOutro(level: levelType | undefined, onOutroEnded: () => void){
   onOrientationData(level: levelType | undefined, v: Vec3){
  
     if(level){
-      console.log(">",level.tracks[0])
+
       const t1 = level.tracks[0]
       const t2 = level.tracks[1]
 
       if(t1 && t2){
+
         const p1 = this.players.tonePlayers.get(t1) 
         const p2 = this.players.tonePlayers.get(t2) 
-        console.log(p1,p2)
         
         if(p1&&p2){
-          console.log(v.y)
+
           p1.volume.value = -( (1-v.y ) * 90) 
           p2.volume.value = -( (v.y) * 90) + 9
         }
