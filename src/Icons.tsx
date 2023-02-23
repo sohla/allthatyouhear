4ac96b43efdcb8dd77a3460dac64e878454cd50c
Steph OHara <soh_la@yahoo.com>
2023-02-24 10:40:56 +1100

    play button bigger

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -16,7 +16,7 @@ export const InfoIcon = (props: {color: string}) => {
 //-----------------------------------------------------------------------
 export const PlayIcon = (props: {color: string}) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={props.color}className="w-20 h-20">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={props.color}className="w-24 h-24">
       <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clipRule="evenodd" />
     </svg>
   )
